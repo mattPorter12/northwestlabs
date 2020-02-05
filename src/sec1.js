1ea7cec960d8febbfadc43cafbe853497087de78
Matthew Scott Porter <mporte22@byu.edu>
2020-02-05 10:51:17 -0700

    added the media files

---
+++ src/sec1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as bs from 'react-bootstrap'
+import { hydrate } from 'react-dom'
+
+export default function SECTION(props) {
+    return (
+        <bs.Button variant = "outline-success" size="lg">
+            hey
+        </bs.Button>
+        //look up about product cards
+    )
+}
